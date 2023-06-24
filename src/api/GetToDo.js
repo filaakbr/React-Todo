aeb7bf486e1152e05d0b8defe75aa7967f40310c
filaakbar <112999117+filaakbr@users.noreply.github.com>
2023-06-24 21:46:08 +0700

    Add files via upload

---
+++ src/api/GetToDo.js
@@ -0,0 +1,11 @@
+import { gql } from "@apollo/client";
+const GET_TODOS = gql`
+  query GetToDos {
+    todo {
+      completed
+      id
+      task
+    }
+  }
+`;
+export default GET_TODOS;
---
+++ src/pages/Login.js
@@ -0,0 +1,101 @@
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const navigate = useNavigate();
+  const handleLoginClick = () => {
+    navigate("/");
+  };
+  return (
+    <>
+      {/*
+          This example requires updating your template:
+  
+          ```
+          <html class="h-full bg-white">
+          <body class="h-full">
+          ```
+        */}
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://img.icons8.com/external-flaticons-flat-flat-icons/64/external-user-web-flaticons-flat-flat-icons-2.png"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Masuk Ke Akun Anda...!!!
+          </h2>
+        </div>
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Masukan Email
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Masukan Sandi
+                </label>
+                <div className="text-sm">
+                  <a
+                    href="/swapi-people"
+                    className="font-semibold text-blue-400 hover:text-green-500"
+                  >
+                    Lupa Sandi?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-blue-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={handleLoginClick}
+              >
+                Masuk
+              </button>
+            </div>
+          </form>
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Tidak Punya Akun?{" "}
+            <a
+              href="#"
+              className="font-semibold leading-6 text-blue-400 hover:text-green-500"///hover:bg-green-400
+            >
+              Coba gratis selama 14 hari
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Login;
